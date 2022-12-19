aa5f9671686520a928030c4e527488f46efd59e2
Kalyvas Emmanouil <emkalyvas@mnapps.net>
2022-12-19 19:25:13 +0200

    config-overrides.js not needed anymore

--- client/config-overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = function override (config, env) {
-    console.log('override')
-    let loaders = config.resolve
-    loaders.fallback = {
-        // existing configs...
-        "os": require.resolve("os-browserify/browser"),
-   }
-    return config
-}
